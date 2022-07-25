70580b6a2b4fcaad4a139dfc7908cae0587e7061
Priyanka-Shahi <shahipriyan@gmail.com>
2022-07-25 20:18:39 +1000

    filter

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution here
+function findMatching(drivers, name) {
+
+  const newDrivers = drivers.filter(function (driver) {
+    return name.toLowerCase() === driver.toLowerCase()});
+  
+
+return newDrivers;}
+
+
+
+
+function fuzzyMatch(drivers, name) {
+  let num = name.length;
+
+  const newDrivers = drivers.filter(
+    function(driver){
+      return name.toLowerCase() === driver.toLowerCase().substring(0,num)
+    })
+
+  return newDrivers;
+}
+
+
+
+function matchName(drivers, name){
+  const newDrivers = drivers.filter(
+    function(driver){
+      return driver.name.toLowerCase() === name.toLowerCase();
+
+    })
+  return newDrivers;
+}
+
